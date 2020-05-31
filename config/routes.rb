cdbe275bd84050f0780e5a23bc780835ff9ef135
Eduardo <jecruxiz@gmail.com>
2020-05-31 18:25:18 -0500

    :sparkles: Add admin user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   
   resources :articles
   root 'articles#index'
-
+  
   get 'signup', to: 'users#new'
   
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   delete 'logout', to: 'sessions#destroy'
-
-
   resources :users, except: [:new]
 end
