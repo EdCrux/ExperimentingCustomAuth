c2c58f85eb16433421a377f6103274261ddca0bd
Eduardo <jecruxiz@gmail.com>
2020-05-30 23:13:19 -0500

    :sparkles: Add authentication part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    #if currrent user already exits return it, if not hit the database and find by id using the session hash
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    #change a value into true or false
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+     flash[:danger] = "You must be logged in to perform that action"
+     redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  
   resources :articles
   root 'articles#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'signup', to: 'users#new'
   
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: 'sessions#destroy'
+
+
   resources :users, except: [:new]
 end
